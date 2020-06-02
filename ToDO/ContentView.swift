4c5ec956a9a89ef0e2d856f40648117bff445cab
Evgeny Bunos <evgeny.bunos@wrenkitchens.com>
2020-06-02 15:47:57 +0300

    Initial Commit

---
+++ ToDO/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDO
+//
+//  Created by Yauheni Bunas on 6/2/20.
+//  Copyright © 2020 Yauheni Bunas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
